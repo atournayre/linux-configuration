6dc2dfa5a8e308a85a32180ca3c4b1a8c3238aef
atournayre <aurelien.tournayre@gmail.com>
2021-03-04 01:08:04 +0100

    Installation script

--- install.sh
+++ install.sh
@@ -15,35 +15,35 @@ check_if_sudo
 . ./scripts/install_ssh.sh --source-only
 . ./scripts/install_phpstorm.sh --source-only
 
-#title 'HTOP'
-#install_htop
-#
-#title 'CHROME'
-#install_chrome
-#
-#title 'TERMINATOR'
-#install_terminator
-#
-#title 'GIT'
-#install_git
-#
-#title 'DOCKER'
-#install_docker
-#
-#title 'DOCKER-COMPOSE'
-#install_docker_compose
-#
-#title 'ZSH'
-#install_zsh
-#
-#title 'CUSTOM TERMINAL'
-#install_lsd
+title 'HTOP'
+install_htop
+
+title 'CHROME'
+install_chrome
+
+title 'TERMINATOR'
+install_terminator
+
+title 'GIT'
+install_git
+
+title 'DOCKER'
+install_docker
+
+title 'DOCKER-COMPOSE'
+install_docker_compose
+
+title 'ZSH'
+install_zsh
+
+title 'CUSTOM TERMINAL'
+install_lsd
 
 title 'SSH Key'
 install_ssh
 
-#title 'PHPStorm'
-#install_phpstorm
+title 'PHPStorm'
+install_phpstorm
 
 install_complete
 exit 0
