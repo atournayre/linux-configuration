2d137ed46e5ed03a0df76227581b972164d8e4cf
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2021-03-08 21:16:04 +0100

    Update install_zsh.sh

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -2,7 +2,7 @@
 
 install_zsh()
 {
-  #  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
   update
   upgrade
