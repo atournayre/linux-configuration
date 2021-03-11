5c2085aa9208c004f026d4249cde2c3347880ecd
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2021-03-11 19:07:55 +0100

    Update install_docker_compose.sh

--- scripts/install_docker_compose.sh
+++ scripts/install_docker_compose.sh
@@ -3,5 +3,8 @@
 install_docker_compose()
 {
   sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
+  
+  sudo chmod +x /usr/local/bin/docker-compose
+  
+  docker-compose --version
 }
